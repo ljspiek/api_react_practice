2d2a8091c4eba810268485a774769497efe88bdd
Laura Spieker <ljspiek@gmail.com>
2019-12-20 20:36:22 -0600

    working country selector & response

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import DemonymApp from './demonymapp/demonymApp'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <DemonymApp />
+      </div>
+    )
+  }
+}
+
+export default App;
